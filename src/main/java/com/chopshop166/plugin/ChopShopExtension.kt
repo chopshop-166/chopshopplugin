f44f40ee46316f2d394a98dc7db0c137ef8174d6
Matt Soucy <git@msoucy.me>
2024-01-17 20:38:14 -0500

    feat: Remove git functionality
    It's better to use a plugin designed for that purpose

--- src/main/java/com/chopshop166/plugin/ChopShopExtension.kt
+++ src/main/java/com/chopshop166/plugin/ChopShopExtension.kt
@@ -10,32 +10,7 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 open class ChopShopExtension(val project : Project) {
-    var version = "2023.2.2"
-
-    fun setupManifest() : Action<Manifest> {
-        fun List<String>.runCommand(currentWorkingDir: File = File("./")): String {
-            val byteOut = ByteArrayOutputStream()
-            project.exec {
-                workingDir = currentWorkingDir
-                commandLine = this@runCommand
-                standardOutput = byteOut
-            }
-            return String(byteOut.toByteArray()).trim()
-        }
-
-        val getGitHash = listOf("git", "describe", "--always").runCommand()
-        val getGitBranch = listOf("git", "rev-parse", "--abbrev-ref", "HEAD").runCommand()
-        val getGitFilesChanged = listOf("git", "diff", "--name-only", "HEAD").runCommand().replace("\n", ", ")
-
-        return Action<Manifest> {
-            attributes(mapOf(
-                "Git-Hash" to getGitHash,
-                "Git-Branch" to getGitBranch,
-                "Git-Files" to getGitFilesChanged,
-                "Build-Time" to LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
-            ))
-        }
-    }
+    var version = "2024.1.3"
 
     fun deps(vararg libs : String) = (listOf("core") + libs).map { "com.chopshop166.chopshoplib:${it}:${version}" }
 }
